7765ded677e886994e1a3c0f66b520cb1f3df128
sevenjra <sevenjra@gmail.com>
2022-06-28 21:01:40 -0300

    finished task

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+import "./Counter.css";
+
+const Counter = () => {
+  const [ contador, setContador ] = useState(0);
+
+  const handleOnClick = (operador) => {
+    const novoValor = operador === '+'
+        ? contador + 1
+        : contador - 1
+    setContador(novoValor)    
+  };
+  return (
+    <div className="counter">
+      <span>{contador}</span>
+      <button onClick={() => handleOnClick("-")}>-</button>
+      <button onClick={() => handleOnClick("+")}>+</button>
+    </div>
+  );
+};
+
+export default Counter;
